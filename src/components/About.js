44682a7765c18532c331d37e813e1945218781ab
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-07-12 05:27:51 +0300

    hooks

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  
+  return <div id="about">About
+   <h2>About Me</h2>
+      <p>
+        I am a dedicated web developer with a passion for creating clean, efficient, and user-friendly applications. I thrive on problem-solving and am always eager to learn new technologies to enhance my skills.
+      </p>
+      <img src={image} alt="I made this" />
+      </div>;
 }
 
 export default About;
